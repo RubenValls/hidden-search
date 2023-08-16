fc4a72f8746d7f501842b6b7fb6734605cd949ae
Rubén Valls <r.vallsaparici@gmail.com>
2023-08-16 17:32:16 +0200

    hidden search working properly

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ const searchBtn = document.querySelector("#search-btn")
 const inputSearch = document.querySelector("#input")
 
 const openInpunt = () => {
-    inputSearch.style.display = inputSearch.style.display === 'none' ? '' : 'none';
+    console.log(inputSearch.style.width)
+    inputSearch.style.width = inputSearch.style.width === '0px' ? '100%' : '0px';
+    inputSearch.style.padding = inputSearch.style.padding === '0px' ? '20px' : '0px';
 }
 
 searchBtn.addEventListener("click", openInpunt)
